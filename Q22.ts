04a303d02985bf59c71d481c0691fd180913a12b
yusraimran2 <yusraimran12@hotmail.com>
2024-11-12 21:23:07 +0500

    Add files via upload

---
+++ Q22.ts
@@ -0,0 +1,6 @@
+// 22:Intentional Error:If you haven't received an array index erorr in one of your programs yet,try to make one happen.Change an index in one of your programs to produce an index erorr.Make sure you correct the erorr before closing the program.
+
+
+let students:string[] = ["Sana", "Sumera", "Afshan", "Nazia", "Bushra"]
+console.log(students[7])
+console.log(students[4])
